d67e64dbeb5e23460f5060417eb24576ca367063
Martin Kovac <>
2022-03-07 23:18:33 +0100

    added sorting

--- generate-tree.py
+++ generate-tree.py
@@ -17,20 +17,41 @@ with open("sitelist.sort", "wt") as file:
 
 tabdepth = 0
 current_tabs = len(base_url.split('/'))
+url_part_dict = {}
 
-final_urls = []
+# Adding domains to dict according to their split length
 for url in unique_urls:
-    url_parts = url.split("/")
-    url_parts_len = len(url_parts)
-
-    if url_parts_len != current_tabs:
-        tabdepth += 1 if url_parts_len > current_tabs else -1
-        current_tabs = url_parts_len
-
-    tab_padding = "\t" * tabdepth
-    final_urls.append(f"{tab_padding}{url}\n")
+    url_length = len(url.split("/")) - 2
+    
+    if str(url_length) not in url_part_dict:
+        url_part_dict[str(url_length)] = [url]
+    else:
+        url_part_dict[str(url_length)].append(url)
 
 
-with open("sitelist.new", "wt") as file:
-    file.write(f"{base_url}\n")
-    file.writelines(final_urls)
+# Sorting each list in dict
+for key, value in url_part_dict.items():
+    url_part_dict[key] = sorted(value)
+
+
+def url_recursion(url, dict_key: int, file):
+    tab_spaces = "\t" * (dict_key - 2)
+    file.write(f"{tab_spaces}{url}\n")
+    
+    if str(dict_key) not in url_part_dict:
+        return None
+    
+    urls_to_check = url_part_dict[str(dict_key)]
+    
+    for url_to_check in urls_to_check:
+        if url in url_to_check:
+            url_recursion(url_to_check, dict_key + 1, file)
+
+def main():
+    file = open("sitelist.new", "wt")
+    url_recursion(base_url, 2, file)
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
