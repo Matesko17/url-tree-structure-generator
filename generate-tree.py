855e9a7f460c87c995f081b221ba271d5f4859ce
Martin Kovac <>
2022-03-09 20:26:03 +0100

    finished script

--- generate-tree.py
+++ generate-tree.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 # Read list of URLs from file
+from typing import final
+
+
 with open("sitelist.raw", "r") as f:
     urls = f.readlines()
 
@@ -22,7 +25,7 @@ url_part_dict = {}
 # Adding domains to dict according to their split length
 for url in unique_urls:
     url_length = len(url.split("/")) - 2
-    
+
     if str(url_length) not in url_part_dict:
         url_part_dict[str(url_length)] = [url]
     else:
@@ -32,24 +35,53 @@ for url in unique_urls:
 # Sorting each list in dict
 for key, value in url_part_dict.items():
     url_part_dict[key] = sorted(value)
-
-
-def url_recursion(url, dict_key: int, file):
-    tab_spaces = "\t" * (dict_key - 2)
-    file.write(f"{tab_spaces}{url}\n")
     
+
+def url_recursion(url, dict_key: int, file, final_list):
+    # print('rekurze')
+    tab_spaces = "\t" * (dict_key - 2)
+    if url in final_list:
+        return None
+    final_list.append(url)
+    file.write(f"{tab_spaces}{url}\n")
+
     if str(dict_key) not in url_part_dict:
         return None
-    
+
     urls_to_check = url_part_dict[str(dict_key)]
-    
+
+    # TODO: fix že se nikdy nedostane do 6???? urovne v rekurzi
     for url_to_check in urls_to_check:
-        if url in url_to_check:
-            url_recursion(url_to_check, dict_key + 1, file)
+        if (url + '/') in url_to_check:
+            url_recursion(url_to_check, dict_key + 1, file, final_list)
+    
+        if url not in urls_to_check:
+            current_dict = int(dict_key)
+            remaining_dicts = len(url_part_dict) - int(dict_key) + 1 
+            while remaining_dicts >= 1:
+                # print(f"remaing_dicts: {remaining_dicts}")
+                # print(f"remaing_dicts: {current_dict}")
+                # print('while loop')
+                new_urls_to_check = url_part_dict[str(current_dict)]
+
+                for new_url_to_check in new_urls_to_check:
+                    if (url + '/') in new_url_to_check:
+                        url_recursion(new_url_to_check, current_dict + 1, file, final_list)
+
+                current_dict += 1
+                remaining_dicts -= 1
+    list_diff = []
+    for item in unique_urls:
+        if item not in final_list:
+            list_diff.append(item)
+    return list_diff
 
 def main():
     file = open("sitelist.new", "wt")
-    url_recursion(base_url, 2, file)
+    final_list = []
+    list_diff = url_recursion(base_url, 2, file, final_list)
+    print(list_diff)
+    print(len(list_diff))
     file.close()
 
 
